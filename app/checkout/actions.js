d14db9116ba375dc245c95ba92bd0d18ba62420f
Vladimir Vorozbit <vorozbitcrypto7@gmail.com>
2023-10-03 20:42:12 +0200

    Delete unnecessary imports

--- app/checkout/actions.js
+++ app/checkout/actions.js
@@ -1,8 +1,6 @@
 'use server';
 
 import { cookies } from 'next/headers';
-import { getCookie } from '../../util/cookies';
-import { parseJson } from '../../util/json';
 
 export async function removeAllItemFromCookies() {
   // Clear the cart by setting an empty array for the cart items
