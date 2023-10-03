de3c5b6c556a9b42be7ba71d42a2d98815ff6e80
Vladimir Vorozbit <vorozbitcrypto7@gmail.com>
2023-10-03 20:40:38 +0200

    Ensure the prevent submission

--- app/checkout/PaymentCredentialsForm.js
+++ app/checkout/PaymentCredentialsForm.js
@@ -4,10 +4,28 @@ import Link from 'next/link';
 import React from 'react';
 import { removeAllItemFromCookies } from './actions';
 
+// import { useState } from 'react';
+
 export default function PaymentCredentialsForm() {
+  // const [creditCardNumber, setCreditCardNumber] = useState('');
+  // const [expirationDate, setExpirationDate] = useState('');
+  // const [securityCode, setSecurityCode] = useState('');
   const handleConfirmOrder = async () => {
+    /*if (
+      creditCardNumber === '' ||
+      !creditCardNumber.match(/^\d{13,19}$/) ||
+      expirationDate === '' ||
+      !expirationDate.match(/^\d[0-9]*$/) ||
+      securityCode === '' ||
+      !securityCode.match(/^\d[0-9]|[0-9]|[0-9]*$/)
+    ) {
+      alert('Enter a valid credit card number.');
+      return;
+    }
+    */
     await removeAllItemFromCookies();
   };
+
   return (
     <>
       <h3>Payment Information</h3>
@@ -23,6 +41,8 @@ export default function PaymentCredentialsForm() {
             maxLength="19"
             autoComplete="off"
             pattern="[0-9\s]{13,19}"
+            // value={creditCardNumber}
+            // onChange={(e) => setCreditCardNumber(e.target.value)}
             required
           />
         </div>
@@ -38,6 +58,8 @@ export default function PaymentCredentialsForm() {
             maxLength="2"
             autoComplete="off"
             pattern="[0-9]*"
+            // value={expirationDate}
+            // onChange={(e) => setExpirationDate(e.target.value)}
             required
           />
         </div>
@@ -54,16 +76,18 @@ export default function PaymentCredentialsForm() {
             maxLength="3"
             autoComplete="off"
             pattern="([0-9]|[0-9]|[0-9])"
+            // value={securityCode}
+            // onChange={(e) => setSecurityCode(e.target.value)}
             required
           />
         </div>
+        <br />
+        <Link data-test-id="checkout-confirm-order" href="/thankYouPage">
+          <div>
+            <button onClick={handleConfirmOrder}>Confirm order!</button>
+          </div>
+        </Link>
       </form>
-      <br />
-      <Link data-test-id="checkout-confirm-order" href="/thankYouPage">
-        <div>
-          <button onClick={handleConfirmOrder}>Confirm order!</button>
-        </div>
-      </Link>
     </>
   );
 }
